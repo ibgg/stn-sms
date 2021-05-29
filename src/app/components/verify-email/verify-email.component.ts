ff5e209bf3851af075f2bdacbe78025534c78b83
Israel Garcia <ibgg11@gmail.com>
2021-05-29 13:10:28 -0600

    Verification email fixes

--- src/app/components/verify-email/verify-email.component.ts
+++ src/app/components/verify-email/verify-email.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { auth } from 'firebase';
 import { AuthService } from '../../shared/services/auth/auth.service';
 
 @Component({
@@ -7,9 +8,19 @@ import { AuthService } from '../../shared/services/auth/auth.service';
   styleUrls: ['./verify-email.component.scss']
 })
 export class VerifyEmailComponent implements OnInit {
+  email:string;
+  user: any;
 
   constructor(public authService: AuthService) { 
 	this.authService.error = "";
+
+	authService.afAuth.currentUser.then(user => {
+		this.email = user.email;
+		this.user = user;
+		console.log('user: ',this.user);
+	}).catch(error => {
+		console.log(error);
+	});
   }
 
   ngOnInit(): void {
